6a0d000d07faf615bfe6bd00cbba019eba3a1be9
enomarozi <marozieno0@gmail.com>
2024-09-27 15:16:13 +0700

    27-09-2024

--- index/views_png.py
+++ index/views_png.py
@@ -10,9 +10,13 @@ def analisaPNG(request, id_):
 		"title":"Analysis PNG",
 		"signature":signature(file),
 		"IHDR":IHDR(file),
+		"sRGB":sRGB(file),
+		"gAMA":gAMA(file),
+		"pHYs":pHYs(file),
 		"IDAT":IDAT(file),
-	}
+		"DATA":DATA(file),
 
+	}
 	return render(request, 'index/analisa_png.html',context)
 
 def signature(file):
@@ -20,20 +24,42 @@ def signature(file):
 	return marker
 
 def IHDR(file):
-	list = []
-	size_ = file.split(b"IHDR")[0][-4:]
-	type_ = b"IHDR"
-	data_ = file.split(b"IHDR")[1][:int.from_bytes(size_,byteorder='big')]
-	crc_ = binascii.crc32(type_+data_) & 0xFFFFFFFF
-	status = "CRC IHDR False"
-	if bytes.fromhex(hex(crc_)[2:]) in file:
-		status = "CRC IHDR True"
-	field_IHDR = {"Width":4, "Height":4,"Size of Color":1,
+	field_marker = {"Width":4, "Height":4,"Size of Color":1,
 				"Color Type":1,"Compression Method":1,"Filter Method":1,
 				"Enclacement Method":1}
+	type_ = b"IHDR"
+	return process(file,field_marker, type_, "IHDR")
+	
+def sRGB(file):
+	field_marker = {"Rendering Intent":1}
+	type_ = b"sRGB"
+	return process(file,field_marker, type_, "sRGB")
+
+def gAMA(file):
+	field_marker = {"Rendering Intent":4}
+	type_ = b"gAMA"
+	return process(file,field_marker, type_, "gAMA")
+
+def pHYs(file):
+	field_marker = {"Pixels per unit X axis":4,"Pixels per unit Y axis":4,
+				  "Unit specifier":1}
+	type_ = b"pHYs"
+	return process(file,field_marker, type_, "pHYs")
+
+def process(file, field_marker, type_, status_):
+	list = []
+	if type_ in file:
+		size_ = file.split(type_)[0][-4:]
+	else:
+		return "'Not Found'"
+	data_ = file.split(type_)[1][:int.from_bytes(size_,byteorder='big')]	
+	crc_ = binascii.crc32(type_+data_) & 0xFFFFFFFF
+	status = "CRC "+status_+" FALSE"
+	if bytes.fromhex(hex(crc_)[2:].rjust(8,'0')) in file:
+		status = "CRC "+status_+" TRUE"
 	data = [hex(i)[2:].rjust(2,'0') for i in data_]
 	start, end = 0,0
-	for field,size in field_IHDR.items():
+	for field,size in field_marker.items():
 		end += size
 		list.append(field+" : "+str(int("".join(data[start:end]),16)))
 		start += size
@@ -52,7 +78,26 @@ def IDAT(file):
 			if check in file:
 				crc_list.append(''.join([hex(j)[2:].rjust(2,'0') for j in check]))
 			else:
-				crc_list.append(check+" not found")
+				crc_list.append(''.join([hex(j)[2:].rjust(2,'0') for j in check])+" Not Found")
 		except:
 			pass
-	print(crc_list)
+	return crc_list
+
+def DATA(file):
+	print("Panjang file :",len(file))
+	result = 0
+	marker = [b"IHDR",b"sRGB",b"gAMA",b"pHYs"]
+	result += 8
+	for i in marker:
+		data_length = int.from_bytes(file.split(i)[0][-4:],byteorder='big')
+		result += 4+4+data_length+4
+	print("result :",result)
+	idat = file.split(b"IDAT")
+	for i in range(len(idat)+1):
+		try:
+			size_ = int(idat[i][-4:].hex(),16)
+			print(hex(size_))
+			result += size_
+		except:
+			pass
+	#print(result)
